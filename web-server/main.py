878095272f8e4b15379a25d4df0aa9311dfa6844
Marco Candia <mtro.candia@gmail.com>
2022-12-01 20:01:50 -0600

    Cambios en Main y requirements de web-server

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,30 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/")
+def get_list():
+    return [1, 2, 3]
+
+@app.get("/contact",response_class=HTMLResponse)
+async def read_items():
+    return """
+    <html>
+        <head>
+            <title>Contactos</title>
+        </head>
+        <body>
+            <h1>Compren</h1>
+            <p>No se hagan weyes</p>
+        </body>
+    </html>
+    """
+#def get_list():
+#    return {"name":"Marco"}
+
+
 
 def run():
     store.get_categories()
